b1258544448d08d5e1fe7394ae452fee8ffb559c
Mirmaxamadov04 <d@mirmaxamadov.gmail.com>
2024-08-22 10:48:32 +0500

    bug fix

--- src/components/Section6/section6.jsx
+++ src/components/Section6/section6.jsx
@@ -14,41 +14,38 @@ import "./section6.css";
 function Section6() {
   const tablet = useMediaQuery("(min-width:701px)");
   return (
-    <>
-      <div className="section6">
-        <InfoSection
-          bigSpan={true}
-          span="#4"
-          heading={
-            <>
-              Biz har kuni{" "}
-              <span>
-                100 dan ortiq ulanish uchun <br /> so'rovlar
-              </span>{" "}
-              qabul qilamiz
-            </>
-          }
-          paragraph={
-            <>
-              Har kuni <span>O'zbekistonning barcha viloyatlaridan</span> 100
-              dan ortiq do'konlar <br /> BILLZ-ga ulanish uchun so'rov
-              qoldirishadi
-            </>
-          }
-        />
-        <div className="image-div">
-          <Image src={firstImage} width="244" height="296" />
-          <Image src={secondImage} width="244" height="296" />
-          <Image src={thirdImage} width="244" height="296" />
-          <Image src={fourthImage} width="244" height="296" />
-        </div>
-        <div>
-          <MyButton primary={true} isOpen={tablet ? true : false}>
-            Konsultatsiya uchun so'rov qoldirish
-          </MyButton>
-        </div>
+    <div className="section6">
+      <InfoSection
+        bigSpan={true}
+        span="#4"
+        heading={
+          <>
+            Biz har kuni{" "}
+            <span>
+              100 dan ortiq ulanish uchun <br /> so'rovlar
+            </span>{" "}
+            qabul qilamiz
+          </>
+        }
+        paragraph={
+          <>
+            Har kuni <span>O'zbekistonning barcha viloyatlaridan</span> 100 dan
+            ortiq do'konlar <br /> BILLZ-ga ulanish uchun so'rov qoldirishadi
+          </>
+        }
+      />
+      <div className="image-div">
+        <Image src={firstImage} width="244" height="296" />
+        <Image src={secondImage} width="244" height="296" />
+        <Image src={thirdImage} width="244" height="296" />
+        <Image src={fourthImage} width="244" height="296" />
       </div>
-    </>
+      <div>
+        <MyButton primary={true} isOpen={tablet ? true : false}>
+          Konsultatsiya uchun so'rov qoldirish
+        </MyButton>
+      </div>
+    </div>
   );
 }
 
